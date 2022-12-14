6a8c750e13efae0e227bc8c537adf885c2b2674a
uijin <uijin9873@gmail.com>
2022-12-15 02:29:25 +0900

    add front_end

---
+++ front_end/login.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const id = document.querySelector("#id"), 
+    password = document.querySelector("#password"),
+    loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", login);
+
+function login() {
+    const request = {
+        id : id.value,
+        password : password.value,
+    };
+    console.log(request);
+}
+fetch('/login',{
+  method: "POST"
+  headers: {
+  	"Content-Type": "application/json"
+  },
+  body: JSON.stringify(request)
+});
