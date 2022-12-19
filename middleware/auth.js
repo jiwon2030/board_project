bd97c7382830c08bc06ece2e86b4bbce8274ba19
Jiwon <jiwon5568@gmail.com>
2022-12-19 22:22:33 +0900

    modified files

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,13 @@ const { User } = require("../models/user")
 
 let auth = (req, res, next) => {
     // 인증 처리를 하는 곳
-
-
     // client cookie에서 token을 가져온다.(cookieParser 이용)
     let token = req.cookies.x_auth
 
     // token을 decode한 후 사용자를 찾는다.
     User.findByToken(token, (err, user) => {
         // 사용자가 없다면 에러 메시지 출력
-        if(err) throw err
+        if(err) throw err;
         if(!user) return res.json({ isAuth: false, error: true })
 
         //사용자가 있다면 token과 user를 req에 저장하고 main.js로 이동
@@ -23,3 +21,5 @@ let auth = (req, res, next) => {
 
     // 사용자가 없으면 인증 실패
 }
+
+module.exports = { auth };
